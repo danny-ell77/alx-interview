3696da44f8328a3f585daa224256c31ceafc9690
Olah Daniel <danielolahskybrow77@gmail.com>
2023-07-02 21:22:33 +0100

    fixed bugs

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,46 +1,28 @@
 #!/usr/bin/python3
-"""A module for working with Pascal's triangle.
-"""
+"""Pascal triangle"""
 
 
-def pascals_triangle(n):
-    """Creates a list of lists of integers representing
-    the Pascal's triangle of a given integer.
+def pascal_triangle(n):
+    """Returns a list of lists of integers
+    representing the Pascal’s triangle of n:
     """
-    traingle = []
-    if not n > 0:
-        return
-    for i in range(n):
-        if i == 0:
-            traingle.append([1])
-        elif i == 1:
-            traingle.append([1, 1])
-        else:
-            new_row = []
-            prev_row = traingle[i - 1]
-            for j in range(i):
-                if j == 0:
-                    new_row.append(1)
-                elif j + 1 == i:
-                    new_row.append(prev_row[j - 1] + prev_row[j])
-                    new_row.append(1)
+
+    if n <= 0:
+        return []
+
+    """ initialize an empty resulting array """
+    pascal = [[] for idx in range(n)]
+
+    for li in range(n):
+        for col in range(li + 1):
+            if col < li:
+                if col == 0:
+                    """the first column is always set to 1"""
+                    pascal[li].append(1)
                 else:
-                    new_row.append(prev_row[j - 1] + prev_row[j])
-            traingle.append(new_row)
+                    pascal[li].append(pascal[li - 1][col] + pascal[li - 1][col - 1])
+            elif col == li:
+                """the diagonal is always set to 1"""
+                pascal[li].append(1)
 
-    return traingle
-
-
-# def pascals_triangle(n):
-#     triangle = []
-#     for i in range(n):
-#         row = [1] * (i + 1)  # Initialize each row with 1s
-#         if i >= 2:
-#             prev_row = triangle[i - 1]
-#             for j in range(1, i):
-#                 row[j] = prev_row[j - 1] + prev_row[j]
-#         triangle.append(row)
-#     return triangle
-
-
-# pprint(pascals_triangle(7))
+    return pascal
